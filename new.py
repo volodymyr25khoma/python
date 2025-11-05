e936325bcec4ed30c17a2803cd747f0c9611a665
Volodymyr Khoma <volodymyr.khoma.mkisp.2025@lpnu.ua>
2025-11-05 12:28:39 +0100

    lesson 1 basic python

--- new.py
+++ new.py
@@ -1,1 +1,14 @@
 print("hello world")
+user_email = "example@gmail.com"
+name = "Volodya"
+print(name)
+
+name = "Volodymyr"
+print(name)
+
+lst = {1, 2, 3, 4, 5}
+dct = {"name": "Volodya", "age": 3}
+name = "Volodya"
+tpl = ("n", "a", "g")
+
+print(dct["name"] == name and dct["age"] in lst)
