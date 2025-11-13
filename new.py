741022085c75597513f04d39852907f095e3d034
Volodymyr Khoma <volodymyr.khoma.mkisp.2025@lpnu.ua>
2025-11-13 21:36:38 +0100

    lesson 7 working with modules

--- new.py
+++ new.py
@@ -19,3 +19,52 @@ for name, age in user_data.items():
 
 for start_pos, last_pos in range_list:
     print_numbers(start_pos, last_pos)
+
+
+# комбіновані умови
+def check_connection(username, count_tries, priority):
+    if priority >= 10:
+        finish = 5
+        for attempt in range(1, count_tries + 1):
+            if attempt == finish:
+                print("Succesful connect!")
+                break
+            print(f"Attempt № {attempt} to connect to {username}")
+
+    elif priority >= 5 and priority < 10:
+        finish = 3
+        for attempt in range(1, 6):
+            if attempt == finish:
+                print("Succesful connect!")
+            print(f"Attempt № {attempt} to connect to {username}")
+
+    else:
+        print("Sorry, but your priority is too low")
+
+
+check_connection(count_tries=10, username="Volodya", priority=20)
+
+
+# модулі
+# створив свій модуль і підключив його у окремій папці
+import turtle
+
+
+def draw(size, color):
+    turtle.speed(1)
+    turtle.color(color)
+    turtle.begin_fill()
+
+    def move(len):
+        turtle.forward(len)
+        turtle.left(90)
+
+    for _ in range(4):
+        move(size)
+
+    turtle.end_fill()
+
+
+draw(100, "green")
+turtle.goto(100, 100)
+draw(200, "purple")
